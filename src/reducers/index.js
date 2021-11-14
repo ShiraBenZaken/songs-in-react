3a6d372d855169051bc18a91247849e7c756ecea
SHIRA BEN ZAKEN <shira441192@gmail.com>
2021-11-14 19:28:10 +0200

    Create index.js
    some reducers lesson 238

---
+++ src/reducers/index.js
@@ -0,0 +1,28 @@
+
+const songsReducer = () => {
+    return [
+        {
+            title: 'Can I be him',
+            duration: '4:06'
+        },
+        {
+            title: 'Impossible',
+            duration: '3:29'
+        },
+        {
+            title: 'Hello',
+            duration: '4:15'
+        },
+        {
+            title: 'Sorry',
+            duration: '3:40'
+        }
+    ]
+};
+
+const selectionSongReducer = (selectedSong = null, action) => {
+    if(action.type === 'SONG_SELECTED') {
+        return action.payload;
+    }
+    return selectedSong;
+};
